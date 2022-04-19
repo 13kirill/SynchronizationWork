f335c3f3a50ea58679de28dbedab21887e17a818
Kirill Zubkov <kirill131313@gmail.com>
2022-04-19 14:07:18 +0300

    Task 1. Программа периодически падает с InterruptedException, потому что когда мы пытаемся остановить поток carFactory он спит. Если у carFactory в методе run сделать достаточно короткий сон, то программа иногда завершается успешно.

--- src/Seller.java
+++ src/Seller.java
@@ -1,37 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Seller {
 
-    private CarShowroom carShowroom;
-    final int CAR_DELIVERY = 3000;
-    final int CAR_ACCEPT = 2000;
+    final int CAR_DELIVERY = 1000;
+    final int CAR_ACCEPT = 500;
+    List<Car> cars = new ArrayList<>(10);
 
-    public Seller(CarShowroom carShowroom) {
-        this.carShowroom = carShowroom;
-    }
-
-    public synchronized Car buyCar() {
+    public synchronized void buyCar() {
         try {
-
             System.out.println(Thread.currentThread().getName() + " заходит в автосалон ВАЗ");
-            while (carShowroom.getCar().size() == 0) {
+            while (cars.size() == 0) {
                 System.out.println("Похоже что у вас в наличии нет автомобилей! Сколько мне ждать?");
-
                 wait();
             }
             Thread.sleep(CAR_DELIVERY);
             System.out.println(Thread.currentThread().getName() + " уехал на новенькой VESTE за 2,5 млн. руб.");
+            cars.remove(0);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        return carShowroom.getCar().remove(0);
     }
 
-
-    public synchronized void acceptCar() {
+    public synchronized void acceptCar(Car car) {
         try {
             System.out.println(Thread.currentThread().getName() + ": скоро должен приехать автовоз.");
+            cars.add(car);
             Thread.sleep(CAR_ACCEPT);
-            carShowroom.getCar().add(new Car());
-            System.out.println(Thread.currentThread().getName() + ": Ура! Приехали новые VESTA!");
+            System.out.println(Thread.currentThread().getName() + ": Ура! Приехала новая VESTA!");
             notify();
         } catch (InterruptedException e) {
             e.printStackTrace();
