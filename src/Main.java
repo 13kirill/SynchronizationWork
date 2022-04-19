da0793a85e3bbfaa35cd74550bd53c3fdc7d36f6
Kirill Zubkov <kirill131313@gmail.com>
2022-04-19 14:40:48 +0300

    Task 2. с InterruptedException такие же проблемы.

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,27 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        final CarShowroom carShowroom = new CarShowroom();
+
+        Seller seller = new Seller();
+
+        CarFactory carFactory = new CarFactory(null, "АвтоВаз", seller);
 
         ThreadGroup buyers = new ThreadGroup("Покупатели");
 
-        for (int i = 1; i <= 3 ; i++) {
-            new Thread(buyers, carShowroom::buyCar, "Покупатель " + i).start();
 
+
+        for (int i = 1; i <= 5 ; i++) {
+            new Thread(buyers, seller::buyCar, "Покупатель " + i).start();
         }
 
-        new Thread(null, carShowroom::acceptCar, "Продавец").start();
+        carFactory.start();
+
+        while (buyers.activeCount() != 0) {
+            Thread.sleep(500);
+        }
+
+           while (!carFactory.isInterrupted()) {
+              carFactory.interrupt();
+          }
+
     }
 }
--- src/Seller.java
+++ src/Seller.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Seller {
 
@@ -7,30 +10,39 @@ public class Seller {
     final int CAR_ACCEPT = 500;
     List<Car> cars = new ArrayList<>(10);
 
-    public synchronized void buyCar() {
+    Lock lock = new ReentrantLock(true);
+    Condition condition = lock.newCondition();
+
+    public void buyCar() {
         try {
+            lock.lock();
             System.out.println(Thread.currentThread().getName() + " заходит в автосалон ВАЗ");
             while (cars.size() == 0) {
                 System.out.println("Похоже что у вас в наличии нет автомобилей! Сколько мне ждать?");
-                wait();
+                condition.await();
             }
             Thread.sleep(CAR_DELIVERY);
             System.out.println(Thread.currentThread().getName() + " уехал на новенькой VESTE за 2,5 млн. руб.");
             cars.remove(0);
         } catch (InterruptedException e) {
             e.printStackTrace();
+        } finally {
+            lock.unlock();
         }
     }
 
-    public synchronized void acceptCar(Car car) {
+    public void acceptCar(Car car) {
         try {
+            lock.lock();
             System.out.println(Thread.currentThread().getName() + ": скоро должен приехать автовоз.");
             cars.add(car);
             Thread.sleep(CAR_ACCEPT);
             System.out.println(Thread.currentThread().getName() + ": Ура! Приехала новая VESTA!");
-            notify();
+            condition.signal();
         } catch (InterruptedException e) {
             e.printStackTrace();
+        } finally {
+            lock.unlock();
         }
     }
 }
